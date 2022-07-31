babcb206887ec4975020a1b5a884da273a9b08e1
Jason Ancrum <jason.ancrum00@delta.com>
2022-07-31 01:48:22 -0400

    final push with challenge completed

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# parser-challenge
-Parsing csv as input
+# Coding Challenge
+
+The intent of this coding assignment was to express proficiency in the Go Programming Language by parsing a CSV file given as input with specific requirements being:
+
+* How many users accessed the server?
+* How many uploads were larger than `50kB`?
+* How many times did `jeff22` upload to the server on April 15th, 2020?
+* Include a small `README.md` describing your solution and additional considerations you would make if you had more time.
+
+
+## Project structure
+
+```sh
+    .
+├── README.md
+├── data
+│   └── server_log.csv
+└── main.go
+
+```
+
+
+## How It Works
+
+1. Extract the parser challenge and change directory into <b> parser-challenge </b>:
+
+    ```sh
+    $ cd parser-challenge
+    ```
+
+2. Ensure <b>server_log.csv</b> is located under "data/" subdirectory:
+
+    ```sh
+    $ vi data/server_log.csv 
+    ```
+
+3. Run the main go file as entrypoint to program
+
+    ```sh
+    $ go run main.go
+    ```
+
+
+## Expected output rendered in terminal
+
+```sh
+-----------------------------------------------------------------
+
+Uploads over 50kb :  144
+
+Number of events captured in the server stdout :  657
+
+Number of unique users :  6
+
+User jeff22 uploaded to server on April 15th, 2020 :  3  times
+
+
+-----------------------------------------------------------------
+```
--- main.go
+++ main.go
@@ -1,117 +1,92 @@
 package main
 
 import (
-        "encoding/csv"
-        "fmt"
-        "log"
-        "os"
-		"time"
-		"strconv"
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
 )
 
-const (
-	jFound string = "jeff22"
-    dFormat string = "2020-04-15"
-)
+const jFound string = "jeff22"
 
 // User object for handling user event data
 type Event struct {
-        Timestamp string `json:"timestamp"`	
-		Username string `json:"username"`
-		Operation string `json:"operation"`
-		Size int `json:"size"`
+	Timestamp string `json:"timestamp"`
+	Username  string `json:"username"`
+	Operation string `json:"operation"`
+	Size      int    `json:"size"`
 }
 
+//Helper utility funcs for removing duplicate users
 func removeDuplicates(arr []string) []string {
-    usr := map[string]bool{}
-    for i := range arr {
-        usr[arr[i]] = true
-    }
-    filtered := []string{} 
-    for j, _ := range usr {
-        filtered = append(filtered, j)
-    }
-    return filtered
+	usr := map[string]bool{}
+	for i := range arr {
+		usr[arr[i]] = true
+	}
+	filtered := []string{}
+	for j, _ := range usr {
+		filtered = append(filtered, j)
+	}
+	return filtered
 }
-
-// Your solution must be able to answer the following types of questions about the example log:
-// 1. How many users accessed the server?
-// 2. How many uploads were larger than `50kB`?
-// 3. How many times did `jeff22` upload to the server on April 15th, 2020?
-
 
 func main() {
-        // find csv local to project and open
-        file, err := os.Open("data/server_log.csv")
-        if err != nil {
-                log.Fatal(err)
-        }
-        defer file.Close()
+	// find csv local to project and open
+	file, err := os.Open("data/server_log.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-        //Parse the individual comma delimited entries
-        csvReader := csv.NewReader(file)
-        data, err := csvReader.ReadAll()
-        if err != nil {
-                log.Fatal(err)
-        }
+	//Parse the individual comma delimited entries
+	csvReader := csv.NewReader(file)
+	data, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-        var events []Event
-		uploads := 0
-		count := 0
-		jevents := 0
-		
+	var events []Event
+	uploads := 0
+	count := 0
+	jevents := 0
 
-		// Unique users array
-		users := make([]string, len(data))
+	// Unique users array
+	users := make([]string, len(data))
 
-        for _, col := range data {
-				size , _ := strconv.Atoi(col[3])
+	for _, col := range data {
+		size, _ := strconv.Atoi(col[3])
+		//Regex pattern matching on Apr 15 date
+		match, _ := regexp.MatchString("Apr 15", col[0])
 
+		event := Event{Timestamp: col[0],
+			Username:  col[1],
+			Operation: col[2],
+			Size:      size,
+		}
 
-				timeStampString := col[0]
-				layOut := "Jan 2, 2006"
-				timeStamp, err := time.Parse(layOut, timeStampString)
-	   
-				if err != nil {
-						fmt.Println(err)
-						os.Exit(1)
-				}
-	   
-				hr, min, sec := timeStamp.Clock()
-	   
-				fmt.Printf("Clock : [%d]hour : [%d]minutes : [%d] seconds \n", hr, min, sec)
-	   
-				year, month, day := timeStamp.Date()
-	   
-				fmt.Printf("Date : [%d]year : [%d]month : [%d]day \n", year, month, day)
-	   
-		
+		//Conditonal check for uploads exceeding 50kb filesize and operation type
+		if size >= 50 && col[2] == "upload" {
+			uploads++
+			events = append(events, event)
+		}
+		users = append(users, col[1])
+		count++
+
+		//Conditional for user jeff22 specfic operations
+		if col[1] == jFound && col[2] == "upload" && match {
+			jevents++
+		}
+	}
+
+	fmt.Println("\n\n-----------------------------------------------------------------")
+	fmt.Println("\nUploads over 50kb : ", uploads)
+	fmt.Println("\nNumber of events captured in the server stdout : ", count-1)
+	//filter collection by unique users
+	fmt.Println("\nNumber of unique users : ", len(removeDuplicates(users))-2)
+	fmt.Println("\nUser jeff22 uploaded to server on April 15th, 2020 : ", jevents, " times") 
+	fmt.Println("\n\n-----------------------------------------------------------------")
+
 	
-                event := Event{Timestamp: timeStamp,
-                        Username:  col[1],
-                        Operation: col[2],
-                        Size:      size,
-                }
-				
-				if  size >= 50 && col[2] == "upload" {
-					uploads++
-					events = append(events, event)
-				}
-				users = append(users, col[1])
-				count++
-
-				if  col[1] == jFound {
-					jevents++
-				}
-        }
-
-		fmt.Println("\n\n-----------------------------------------------------------------")
-		fmt.Println("\nUploads over 50kb : ", uploads)
-		//filter collection by unique users
-		fmt.Println("\nNumber of events captured in the server stdout : ", count)
-
-    	fmt.Println("\nNumber of unique users : ",  len(removeDuplicates(users))-2)
-
-		fmt.Println("\nUser jeff22 uploaded to server on April 15th, 2020 : ", jevents , " times")  //prettify the output
-		fmt.Println("\n\n-----------------------------------------------------------------")
 }
